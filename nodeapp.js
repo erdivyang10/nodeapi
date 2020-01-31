4c5b4d7ee5b4ccc3f01ac198e2a40aeed5aacf32
Divyang Goswami <erdivyang10@gmail.com>
2020-01-31 18:51:06 +0530

    deploy to heroku

--- nodeapp.js
+++ nodeapp.js
@@ -3,7 +3,7 @@ var app = express()
 var port = process.env.PORT || 5000
 
 app.get('/', function(req,res){
-    res.send('Hello world ! My App is ready to deploy')
+    res.send('Hello world ! My App is ready to deployy')
 })
 
 app.listen(port)
